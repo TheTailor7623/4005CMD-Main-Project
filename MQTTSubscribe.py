395215018c359036a0919727783c06b198545c89
TheTailor7623 <yseyam7623@gmail.com>
2024-02-13 18:45:28 +0000

    Comments included
    I have included the comments in my code that I had missed - they describe what I had learned and modified in my last commit

--- MQTTSubscribe.py
+++ MQTTSubscribe.py
@@ -1,23 +1,39 @@
 import paho.mqtt.client as mqtt
 import time
 
+#Function to be called on recieving a message
 def on_message(client, userdata, message):
+    #Finds out what topic a message is from and attaches it to variable "topic"
     topic = message.topic
+    
+    #Decodes content of message and attaches it to variable "payload"
     payload = message.payload.decode("utf-8")
 
+    #If the topic is Frequency...
     if topic == "Frequency":
+        #Print the decoded content of message "payload" from topic "Frequency"
         print("Recieved frequency: ", payload)
 
+    #If the topic is Fuel...
     if topic == "Fuel":
+        #Print the decoded content of message "payload" from topic "Fuel"
         print("Recieved fuel: ", payload)
-    
+
+#Connects to broker    
 mqttBroker = "mqtt.eclipseprojects.io"
+
+#Creates new client "iPhone"
 client = mqtt.Client("iPhone")
+
+#Connects client "iPhone" to broker
 client.connect(mqttBroker)
 
-client.on_message = on_message
-
+#Subscribe client to topics Frequency and Fuel
 client.subscribe("Frequency")
 client.subscribe("Fuel")
 
+#When client recieves message than run on_message function
+client.on_message = on_message
+
+#Run this loop forever
 client.loop_forever()
--- Publisher.py
+++ Publisher.py
@@ -4,20 +4,34 @@ from datetime import date, datetime
 from random import randrange, uniform
 import time
 
+#Connecting to broker
 mqttBroker = "mqtt.eclipseprojects.io" 
+
+#Creating a new client "New boat prototype"
 client = mqtt.Client("New boat prototype")
 
+#Connecting the new client to the broker
 client.connect(mqttBroker)
 
+#While loop to publish random values
 while True:
+    #Generating random frequency value
     randFrequency = uniform(1.0, 10.0)
 
+    #Creating and publishing to a "Frequency" topic
     client.publish("Frequency", randFrequency)
+
+    #Print statement to inform of published Frequency + timeout
     print("Just published " + str(randFrequency) + " to topic Frequency")
     time.sleep(2)
 
+    #Generating random fuel value
     randFuel = uniform(20.0, 50.0)
+
+    #Creating and publishing to a "Fuel" topic
     client.publish("Fuel", randFuel)
+
+    #Print statement to inform of published "Fuel" + timeout
     print("Just published " + str(randFuel) + " to topic Fuel")
     time.sleep(2)
     
